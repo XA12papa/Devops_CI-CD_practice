127129c595d4dd3f8f72d8936a02abd756261dd2
Bidesh <cideshmukhi@gmail.com>
2025-07-07 13:18:48 +0530

    cheaking the ci/cd pipeline

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -7,8 +7,7 @@ export default async function Home() {
 
   return (
     <div>
-       UserName :
-      {user?.username}
+       UserName : "I just changed this line"
         Password:
       {user?.password}
       <br />
